2f9f4776af2f37574425cfbc7b7d798bd1d17bd0
Nisarg Sah <nisargshah100@gmail.com>
2012-03-08 23:25:01 -0500

    Finished with letters

--- event_manager.rb
+++ event_manager.rb
@@ -1,9 +1,15 @@
 require "csv"
+require "sunlight"
 
 class EventManager
-  def initialize
+  INVALID_ZIPCODE = "00000"
+  HEADING_LINE_CSV = 2
+  VALID_PHONE_NUMBER_LENGTH = 10
+  Sunlight::Base.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+  def initialize(filename)
     puts "EventManager Initialized."
-    @file = CSV.open("event_attendees.csv", {:headers => true, :header_converters => :symbol})
+    @file = CSV.open(filename, {:headers => true, :header_converters => :symbol})
   end
 
   def print_names
@@ -18,11 +24,88 @@ class EventManager
     end
   end
 
-  def clean_number(number)
-    number.gsub(/[^\d]/, '')
+  def clean_number(original)
+    cleaned = original.gsub(/-|\.|\s|\(|\)/, '')
+
+    if cleaned.length == VALID_PHONE_NUMBER_LENGTH
+      cleaned
+    elsif cleaned.length == VALID_PHONE_NUMBER_LENGTH+1 && cleaned[0] == 1
+      cleaned[1..-1]
+    else
+      nil
+    end
+  end
+
+  def print_zipcodes
+    @file.each do |line|
+      zipcode = clean_zipcode(line[:zipcode])
+      puts zipcode
+    end
+  end
+
+  def clean_zipcode(original)
+    zip = ("%05d" % (original || 0))
+    zip unless zip == INVALID_ZIPCODE
+  end
+
+  def output_data(filename)
+    output = CSV.open(filename, "w")
+    @file.each do |line|
+      if @file.lineno == HEADING_LINE_CSV
+        output << line.headers
+      else
+        line[:homephone] = clean_number(line[:homephone])
+        line[:zipcode] = clean_zipcode(line[:zipcode])
+        output << line
+      end
+    end
+  end
+
+  def rep_lookup
+    20.times do
+      line = @file.readline
+      
+      legislators = Sunlight::Legislator.all_in_zipcode(clean_zipcode(line[:zipcode]))
+      names = legislators.collect do |leg|
+        first_name = leg.firstname
+        first_initial = first_name[0]
+        last_name = leg.lastname
+        first_initial + ". " + last_name
+      end
+
+      puts "#{line[:last_name]}, #{line[:first_name]}, #{line[:zipcode]}, #{names.join(", ")}"
+    end
+  end
+
+  def create_form_letters
+    letter = File.open("form_letter.html", "r").read
+    20.times do
+      line = @file.readline
+
+      custom_letter = letter.gsub("#first_name", line[:first_name])
+      custom_letter = custom_letter.gsub("#last_name", line[:last_name])
+      custom_letter = custom_letter.gsub("#city", line[:city])
+      custom_letter = custom_letter.gsub("#state", line[:state])
+      custom_letter = custom_letter.gsub("#zipcode", line[:zipcode]) 
+      custom_letter = custom_letter.gsub("#street", line[:street])
+
+      filename = "output/thanks_#{line[:last_name]}_#{line[:first_name]}.html"
+      output = File.new(filename, "w")
+      output.write(custom_letter)
+    end
+  end
+
+  def rank_times
+    hours = Array.new(24){0}
+    @file.each do |line|
+      hour = line[:regdate]
+      hour = hour.split[1].split(":")[0].to_i
+      hours[hour] += 1
+    end
+    hours.each_with_index{|counter,hour| puts "#{hour}\t#{counter}"}
   end
 
 end
 
-manager = EventManager.new
-manager.print_numbers
+manager = EventManager.new("event_attendees.csv")
+manager.create_form_letters
